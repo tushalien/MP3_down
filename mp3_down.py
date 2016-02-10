98104241f9daf7d1e694020f8c5946fd4e0a1e10
Tushar Agrawal <Tushar Agrawal>
2016-02-10 13:19:28 +0530

    Please enter the commit message for your changes. Lines starting

--- mp3_down.py
+++ mp3_down.py
@@ -1,38 +1,11 @@
 import os 
 import requests
 import bs4
-import urllib
-import re
-http_proxy  = ""
-https_proxy = ""
-ftp_proxy   = ""
-
-proxyDict = { 
-              "http"  : http_proxy, 
-              "https" : https_proxy, 
-              "ftp"   : ftp_proxy
-            }
-artist_name = input('Enter the name of the artist ')
-artist_url = 'http://www.top50songs.org/artist.php?artist='+artist_name	
-
-
-if not os.path.exists(artist_name):
-	os.makedirs(artist_name)
-current = os.getcwd()
-new=current+'\\'+artist_name
-os.chdir(new)
-
-song_url=[]
-
 
 def song_parser(name):
-			
 			link2='https://mp3skull.yoga/mp3/'
-			
 			name=name.replace(' ','_')
-
 			url=link2+name+'.html'
-
 			r  = requests.get(url)
 			data = r.text
 			soup = bs4.BeautifulSoup(data,"html.parser")
@@ -41,9 +14,6 @@ def song_parser(name):
 					temp_name=download_file(item.get('href'),name)
 				break
 				print(temp_name)
-
-
-	
 
 def download_file(url,local_filename):
 				local_filename = local_filename+".mp3"
@@ -59,26 +29,27 @@ def download_file(url,local_filename):
 					for chunk in r.iter_content(chunk_size=1024): 
 						if chunk: 
 							f.write(chunk)
-							
 				return local_filename
 
-
-song_list=[]
-
-
 def main():
+	artist_name = input('Enter the name of the artist ')
+	artist_url = 'http://www.top50songs.org/artist.php?artist='+artist_name
+	if not os.path.exists(artist_name):
+		os.makedirs(artist_name)
+	current = os.getcwd()
+	new=current+'\\'+artist_name
+	os.chdir(new)
+
 	r  = requests.get(artist_url)
 	data = r.text
 	soup = bs4.BeautifulSoup(data,"html.parser")
-
 	for link in soup.find_all("li"):
 		for item in link.find_all("a"):
 			print(item.get('title'))
 			song_parser(item.get('title'))
 
-
-			
-main()			
+if __name__ == '__main__':
+	main()
 		
 	
 
